b40855dbec43d2d62a5675ac85c131436db4fbc2
谢浩杰 <1012351395@qq.com>
2018-09-12 16:12:35 +0800

    增加了按下播放音乐的功能

--- app/src/main/java/xhj/zime/com/beatbox/Sound.java
+++ app/src/main/java/xhj/zime/com/beatbox/Sound.java
@@ -4,11 +4,23 @@ public class Sound {
     private String mAssetPath;
     private String mName;
 
+    //一般都使用对象类型,如果值没有传入会自动填充为null,这个字段是soundPool预加载之后给的Id
+    private Integer soundId;
+
     public Sound(String assetPath) {
         mAssetPath = assetPath;
         String[] components = assetPath.split("/");
         String fileName = components[components.length - 1];
-        mName = fileName.replace(".wav","");
+        mName = fileName.replace(".wav", "");
+    }
+
+
+    public Integer getSoundId() {
+        return soundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        this.soundId = soundId;
     }
 
     public String getAssetPath() {
--- app/src/main/java/xhj/zime/com/beatbox/SoundViewModel.java
+++ app/src/main/java/xhj/zime/com/beatbox/SoundViewModel.java
@@ -2,14 +2,19 @@ package xhj.zime.com.beatbox;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.view.View;
 
 public class SoundViewModel extends BaseObservable{
+
     private Sound mSound;
     private BeatBox mBeatBox;
 
     public SoundViewModel(BeatBox beatBox) {
         mBeatBox = beatBox;
     }
+
 
     public Sound getSound() {
         return mSound;
@@ -23,5 +28,9 @@ public class SoundViewModel extends BaseObservable{
     @Bindable
     public String getTitle(){
         return mSound.getName();
+    }
+
+    public void onButtonClicked(){
+        mBeatBox.play(mSound);
     }
 }
